edfa263a804c428e8ce637554fbe79e39867ba64
atm4god <tmarvellous41@gmail.com>
2024-08-31 22:07:30 +0100

    Add Profile model to Django admin interface
    This commit registers the Profile model in the Django admin site. It includes a custom ProfileAdmin to display user, address, city, and state fields in the admin list view. This enhances manageability of user profiles within the admin dashboard.

--- myApp/form_app/admin.py
+++ myApp/form_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import MyModel
+from .models import MyModel, Profile
 
 
 # Register your models here.
@@ -8,4 +8,9 @@ class MyModelAdmin(admin.ModelAdmin):
     list_display = ('id', 'first_name', 'last_name', 'email')
 
 
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'address', 'city', 'state')
+
+
 admin.site.register(MyModel, MyModelAdmin)
+admin.site.register(Profile, ProfileAdmin)
